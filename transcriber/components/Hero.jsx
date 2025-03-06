8a92bf10a62647fc2f4b47d9b68ab5bee0b85ca7
Max Lasitsa <mlasitsa@css.edu>
2025-03-05 20:34:01 -0800

    basic ui set up, but with some issues

---
+++ transcriber/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroGeometric from "./ui/shape-landing-hero";
+ 
+
+
+const Hero = () => {
+  return (
+    <div>
+        <HeroGeometric />
+    </div>
+  )
+}
+
+export default Hero
